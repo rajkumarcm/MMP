279dae7727cc1a1809f77c35a316bb35b482ea9d
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2022-07-25 22:36:09 -0400

    Issue persist with right edge offset

--- hierarchical_template/hlayout/app.R
+++ hierarchical_template/hlayout/app.R
@@ -9,6 +9,7 @@
 
 library(shiny)
 library(visNetwork)
+# source('generate_xoffset_template.R')
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -79,63 +80,104 @@ ui <- fluidPage(
 # }
 # load('../nodes.RData')
 # load('../edges.RData')
-server <- function(input, output) {
-  
-  # title=c('1_2', '1_3', '2_4', '2_5', '3_6', '3_7')
-  e_year <- c(2010, 2012, 2011, 2011, 2013, 2013)
-  n_year <- c(2010, 2011, 2013, 2012, 2012, 2014, 2014, 2014, 2014)
-  
-  # year_r <- rank(year)
-  # maxlen <- 200
-  # edge_len <- log(year_r, 6) * 200 + 50
-  # browser()
-  nodes <- data.frame(id = 1:9, 
-                      label=paste("Node",1:9), 
-                      title=paste("Node",1:9)#,
-                      # level=n_year
-                      #group=c(2010, 2011, 2013, 2012, 2012, 2014, 2014)
-  )
-  offset <- 30
-  nodes$x <- c(90, 90, 270, 0, 180, 270, 360, 450, 540)
-  nodes$y <- c(0, 150, 450, 300, 300, 600, 600, 600, 600)
+# server <- function(input, output) {
+#   
+#   # title=c('1_2', '1_3', '2_4', '2_5', '3_6', '3_7')
+#   e_year <- c(2010, 2012, 2011, 2011, 2013, 2013)
+#   n_year <- c(2010, 2011, 2013, 2012, 2012, 2014, 2014, 2014, 2014)
+#   
+#   # year_r <- rank(year)
+#   # maxlen <- 200
+#   # edge_len <- log(year_r, 6) * 200 + 50
+#   # browser()
+#   nodes <- data.frame(id = 1:9, 
+#                       label=paste("Node",1:9), 
+#                       title=paste("Node",1:9)#,
+#                       # level=n_year
+#                       #group=c(2010, 2011, 2013, 2012, 2012, 2014, 2014)
+#   )
+#   offset <- 30
+#   nodes$x <- c(90, 90, 270, 0, 180, 270, 360, 450, 540)
+#   nodes$y <- c(0, 150, 450, 300, 300, 600, 600, 600, 600)
+# 
+#   edges <- data.frame(from = c(1, 1, 2, 2, 3, 3, 8), 
+#                       to = c(2, 3, 4, 5, 6, 7, 9),
+#                       title=c('1_2', '1_3', '2_4', '2_5', '3_6', '3_7', '8_9')
+#   )
+#   edges$smooth.roundness <- c(0, 0.56, 0, 0, 0, 0, 0)
+#   # edges$smooth.roundness <- c(0, 0.56, 0, 0, 0, 0, 0)
+#   
+#   # roundness <- c(0)
+#   # start_year <- min(n_year)
+#   # for(i in 2:nrow(edges))
+#   # {
+#   #   node_y <- n_year[i]
+#   #   if(node_y - (start_year + 1) != 0)
+#   #   {
+#   #     n_nodes <- length(n_year[n_year >= start_year+1 & n_year <= node_y])
+#   #     roundness <- c(roundness, log(n_nodes) * 3e-1)
+#   #   }
+#   # }
+#   
+#   #roundness=c(0, 0.36, 0, 0, 0, 0, 0)
+#   
+#   # browser()
+#   output$hierarchical <- renderVisNetwork({
+#     visNetwork(nodes, edges) %>%
+#       # visHierarchicalLayout(direction = "UD", 
+#       #                       sortMethod = 'directed'#,
+#       #                       #parentCentralization = T
+#       #                       ) %>%
+#       visEdges(
+#                smooth = list(enabled=T,
+#                              type='curvedCW',
+#                              forceDirection='horizontal'),
+#                arrows=list(to=list(enabled=T))) %>%
+#       visPhysics(enabled = F)
+#     # visPhysics(hierarchicalRepulsion=list(avoidOverlap=1))
+#   })
+# }
+# load('www/nodes.RData')
+# load('www/edges.RData')
 
-  edges <- data.frame(from = c(1, 1, 2, 2, 3, 3, 8), 
-                      to = c(2, 3, 4, 5, 6, 7, 9),
-                      title=c('1_2', '1_3', '2_4', '2_5', '3_6', '3_7', '8_9')
-  )
-  edges$smooth.roundness <- c(0, 0.56, 0, 0, 0, 0, 0)
-  # edges$smooth.roundness <- c(0, 0.56, 0, 0, 0, 0, 0)
-  
-  # roundness <- c(0)
-  # start_year <- min(n_year)
-  # for(i in 2:nrow(edges))
-  # {
-  #   node_y <- n_year[i]
-  #   if(node_y - (start_year + 1) != 0)
-  #   {
-  #     n_nodes <- length(n_year[n_year >= start_year+1 & n_year <= node_y])
-  #     roundness <- c(roundness, log(n_nodes) * 3e-1)
-  #   }
-  # }
-  
-  #roundness=c(0, 0.36, 0, 0, 0, 0, 0)
-  
-  # browser()
+# UNCOMMENT THIS-------------------------------------------
+server <- function(input, output) {
+
   output$hierarchical <- renderVisNetwork({
     visNetwork(nodes, edges) %>%
-      # visHierarchicalLayout(direction = "UD", 
+      # visHierarchicalLayout(direction = "UD",
       #                       sortMethod = 'directed'#,
       #                       #parentCentralization = T
       #                       ) %>%
       visEdges(
-               smooth = list(enabled=T,
-                             type='curvedCW',
-                             forceDirection='horizontal'),
-               arrows=list(to=list(enabled=T))) %>%
+        # smooth = list(enabled=T,
+        #               type='curvedCW',
+        #               forceDirection='horizontal'),
+        arrows=list(to=list(enabled=T))) %>%
       visPhysics(enabled = F)
     # visPhysics(hierarchicalRepulsion=list(avoidOverlap=1))
   })
 }
+
+# server <- function(input, output){
+#   nodes <- data.frame(id=c(1,2,3,4,5), x=c(90,90,0,90,180), 
+#                       y=c(0, 150, 300, 300, 300))
+#   edges <- data.frame(from=c(1,2,2,2), to=c(2,3,4,5))
+#   output$hierarchical <- renderVisNetwork({
+#     visNetwork(nodes, edges) %>%
+#       # visHierarchicalLayout(direction = "UD",
+#       #                       sortMethod = 'directed'#,
+#       #                       #parentCentralization = T
+#       #                       ) %>%
+#       visEdges(
+#         # smooth = list(enabled=T,
+#         #               type='curvedCW',
+#         #               forceDirection='horizontal'),
+#         arrows=list(to=list(enabled=T))) %>%
+#       visPhysics(enabled = F)
+#     # visPhysics(hierarchicalRepulsion=list(avoidOverlap=1))
+#   })
+# }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
---
+++ hierarchical_template/hlayout/generate_xoffset_template.R
@@ -0,0 +1,137 @@
+rm(list=ls())
+library(dplyr)
+
+get_width <- function(node_id)
+{
+  if(!node_id %in% visited_nodes)
+  {
+	  visited_nodes <<- c(visited_nodes, node_id)
+	  t_edges <- edges[edges$from==node_id, ]
+	  t_nodes <- unique(t_edges$to)
+	  total_child_width <- 0
+	  n_childs <- length(t_nodes)
+	  
+	  if(n_childs > 0)
+	  {
+  	  for(i in 1:length(t_nodes))
+  	  {
+    		child <- t_nodes[i]
+    		tmp_width <- get_width(child)
+    		nodes[nodes$id==child, 'width'] <<- tmp_width
+    		total_child_width <- total_child_width + tmp_width
+    		visited_nodes <<- c(visited_nodes, child)
+  	  }
+	  }
+	  else
+	    return(0) # the width would be 0 for the leaf node
+		
+	  max_width <- max(c(n_childs, total_child_width))
+	  return(max_width)
+	  
+  }
+  else
+	nodes[nodes$id==node_id, 'width']
+}
+
+nodes <- data.frame(id=1:12,
+                    year=c(2010, 2011, 2011, 2012, 2012, 2012, 2012, 2013, 2013, 2013, 2013, 2014))
+edges <- data.frame(from=c(1,2,2,1,3,3,6,6,7,7,10),
+					          to=c(2,4,5,3,6,7,8,9,10,11,12))
+
+visited_nodes <- c()
+
+fill_width <- function()
+{
+  for(i in 1:length(nodes))
+  {
+    node_id <- nodes[i, 'id']
+    nodes[nodes$id==node_id, 'width'] <<- get_width(node_id)
+  }
+}
+
+fill_width()
+
+
+visited_nodes <- c()
+node_spacing <- 90
+
+estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_x)
+{
+  # x -> parent's x corodinate
+  # prev_x -> prev sibling's x coordinate
+  if(! node_id %in% visited_nodes)
+  {
+    visited_nodes <<- c(visited_nodes, node_id)
+    t_edges <- edges[edges$from==node_id, ]
+    t_nodes <- unique(t_edges$to)
+    n_childs <- length(t_nodes)
+    # local_acc <- acc
+    current_width <- nodes[nodes$id==node_id, 'width']
+    current_x <- 0
+    if(n_childs_parent > 1)
+    {
+      current_center <- (current_width*90)/2 - node_spacing/2
+      equal_divide_margin <- x - (node_spacing/2)
+      unequal_divide_margin <- (prev_x+(node_spacing/2))*2 # this should be zero for the left-most node in the graph
+      margin_left <- max(c(equal_divide_margin, unequal_divide_margin))
+      nodes[nodes$id==node_id, 'x'] <<- margin_left + current_center
+      current_x <- margin_left + current_center
+    }
+    else if(nth_child==1 & n_childs_parent==1)
+    {
+      nodes[nodes$id==node_id, 'x'] <<- x
+      current_x <- x
+    }
+    # else
+    # {
+    #   # When nth_child > 1 & n_childs_parent > 1
+    #   
+    # }
+    
+    # For the node that has children
+    if(node_id==4)
+    {
+      print('breakpoint...')
+    }
+    
+    if(n_childs > 0)
+    {
+      for(i in 1:n_childs)
+      {
+        if(i > 1)
+        {
+          prev_child <- t_nodes[i-1]
+          prev_x <- nodes[nodes$id==prev_child, 'x']
+        }
+        
+        child <- t_nodes[i]
+        #node_id, nth_child, n_childs_parent, x, prev_x, acc)
+        estimate_xcoord(child, i, n_childs, current_x, prev_x)
+        # local_acc <- local_acc + (current_width - max(c(1, current_width)))
+      }
+    }
+    else{
+      # Do not write anything here because
+      # we do not need to encode any information about the main node here
+      # as they are done before the beginning of the loop
+    }
+  }
+  else
+    nodes[nodes$id==node_id, 'x']
+}
+
+tmp <- estimate_xcoord(1, 1, 2, node_spacing, -node_spacing/2)
+
+estimate_ycoord <- function(nodes)
+{
+  y.node_spacing <- 150
+  years <- unique(nodes$year)
+  years <- data.frame(year=years) %>% arrange(year)
+  years$y <- seq(0, by=150, length.out=nrow(years))
+  nodes <- nodes %>% inner_join(years, by="year")
+  return(nodes)
+}
+
+nodes <- estimate_ycoord(nodes)
+# save(file='nodes.RData', 'nodes')
+# save(file='edges.RData', 'edges')
