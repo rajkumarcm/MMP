603413b7b1d53d6c870312facb93f17762073a2d
Rajkumar Conjeevaram Mohan <rajkumarcm@yahoo.com>
2022-07-26 19:58:54 -0400

    Fix offset of disconnected clusters

--- hierarchical_template/hlayout/generate_xoffset_template.R
+++ hierarchical_template/hlayout/generate_xoffset_template.R
@@ -33,10 +33,10 @@ get_width <- function(node_id)
 	nodes[nodes$id==node_id, 'width']
 }
 
-nodes <- data.frame(id=1:12,
-                    year=c(2010, 2011, 2011, 2012, 2012, 2012, 2012, 2013, 2013, 2013, 2013, 2014))
-edges <- data.frame(from=c(1,2,2,1,3,3,6,6,7,7,11),
-					          to=c(2,4,5,3,6,7,8,9,10,11,12))
+nodes <- data.frame(id=1:15,
+                    year=c(2010, 2011, 2011, 2012, 2012, 2012, 2012, 2013, 2013, 2013, 2013, 2014, 2013, 2014, 2014))
+edges <- data.frame(from=c(1,2,2,1,3,3,6,6,7,7,11,13,13),
+					          to=c(2,4,5,3,6,7,8,9,10,11,12,14,15))
 
 visited_nodes <- c()
 
@@ -51,9 +51,18 @@ fill_width <- function()
 
 fill_width()
 
-
 visited_nodes <- c()
 node_spacing <- 90
+global_prev_width <- 0
+global_prev_x <- 0
+
+compute_center <- function(current_width, prev_width, prev_x)
+{
+  current_center <- (max(1, current_width)*90)/2 - node_spacing/2
+  unequal_divide_margin <- (prev_x+(node_spacing/2))+((prev_width*node_spacing)/2) # this should be zero for the left-most node in the graph
+  margin_left <- unequal_divide_margin
+  margin_left + current_center
+}
 
 estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_width,
                             prev_x)
@@ -62,7 +71,7 @@ estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_width,
   # prev_x -> prev sibling's x coordinate
   if(! node_id %in% visited_nodes)
   {
-    if(node_id==10)
+    if(node_id==12)
     {
       print('breakpoint...')
     }
@@ -75,17 +84,13 @@ estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_width,
     current_x <- 0
     if(n_childs_parent > 1)
     {
-      current_center <- (max(1, current_width)*90)/2 - node_spacing/2
-      # equal_divide_margin <- x - (node_spacing/2)
-      unequal_divide_margin <- (prev_x+(node_spacing/2))+((prev_width*node_spacing)/2) # this should be zero for the left-most node in the graph
-      # margin_left <- max(c(equal_divide_margin, unequal_divide_margin))
-      margin_left <- unequal_divide_margin
-      # margin_left <- equal_divide_margin
-      # if(current_center==0)
-      #   current_center <- node_spacing/2
-      nodes[nodes$id==node_id, 'x'] <<- margin_left + current_center
-      current_x <- margin_left + current_center
-      
+      # current_center <- (max(1, current_width)*90)/2 - node_spacing/2
+      # unequal_divide_margin <- (prev_x+(node_spacing/2))+((prev_width*node_spacing)/2) # this should be zero for the left-most node in the graph
+      # margin_left <- unequal_divide_margin
+      # nodes[nodes$id==node_id, 'x'] <<- margin_left + current_center
+      # current_x <- margin_left + current_center
+      current_x <- compute_center(current_width, prev_width, prev_x)
+      nodes[nodes$id==node_id, 'x'] <<- current_x
     }
     else if(nth_child==1 & n_childs_parent==1)
     {
@@ -99,15 +104,7 @@ estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_width,
                                             (node_spacing/2)
       current_x <- current_x + (node_spacing/2)
     }
-    # else
-    # {
-    #   # When nth_child > 1 & n_childs_parent > 1
-    #   
-    # }
-    
-    # For the node that has children
 
-    
     if(n_childs > 0)
     {
       for(i in 1:n_childs)
@@ -117,9 +114,15 @@ estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_width,
           prev_child <- t_nodes[i-1]
           prev_x <- nodes[nodes$id==prev_child, 'x']
           prev_width <- nodes[nodes$id==prev_child, 'width']
+          global_prev_width <<- prev_width
+          global_prev_x <<- prev_x
         }
         
         child <- t_nodes[i]
+        if(node_id==7 & child==11)
+        {
+          print('breakpoint...')
+        }
         #node_id, nth_child, n_childs_parent, x, prev_x, acc)
         estimate_xcoord(child, i, n_childs, current_x, prev_width, prev_x)
         # local_acc <- local_acc + (current_width - max(c(1, current_width)))
@@ -131,11 +134,55 @@ estimate_xcoord <- function(node_id, nth_child, n_childs_parent, x, prev_width,
       # as they are done before the beginning of the loop
     }
   }
-  else
-    nodes[nodes$id==node_id, 'x']
+  # else
+  #   nodes[nodes$id==node_id, 'x']
+}
+#node_id, nth_child, n_childs_parent, x, prev_width, prev_x
+# tmp <- estimate_xcoord(1, 1, 1, 225, 0, 0)
+
+get_prev_width <- function(node_id)
+{
+  width <- 0
+  partition1 <- nodes[nodes$id < node_id,]
+  if(nrow(partition1) > 0)
+  {
+    unique_years <- unique(partition1$year)
+    min_year <- min(unique_years)
+    width <- 0
+    prior_roots <- partition1[partition1$year==min_year, 'id']
+    for(i in 1:length(prior_roots))
+    {
+      root_nid <- prior_roots[i]
+      width <- width + nodes[nodes$id==root_nid, 'width']
+    }
+  }
+  width
 }
 
-tmp <- estimate_xcoord(1, 1, 2, 360, 0, -node_spacing/2)
+fill_xcoord <- function()
+{
+  for(i in 1:nrow(nodes))
+  {
+    node_id <- nodes[i, 'id']
+    current_width <- nodes[i, 'width']
+    if(node_id==1)
+    {
+      print('breakpoint...')
+    }
+    t_edges <- edges[edges$from==node_id, ]
+    t_nodes <- unique(t_edges$to)
+    n_childs <- length(t_nodes)
+    
+    prev_width <- max(global_prev_width, get_prev_width(node_id), na.rm=T)
+    prev_x <- global_prev_x
+    x_current <- compute_center(prev_x=prev_x, prev_width=prev_width,
+                                current_width=current_width)
+    estimate_xcoord(node_id=node_id, nth_child=1, n_childs_parent=1,
+                    x=x_current, prev_width=prev_width, prev_x=prev_x)
+  }
+}
+
+fill_xcoord()
 
 estimate_ycoord <- function(nodes)
 {
@@ -146,6 +193,7 @@ estimate_ycoord <- function(nodes)
   nodes <- nodes %>% inner_join(years, by="year")
   return(nodes)
 }
+
 
 nodes <- estimate_ycoord(nodes)
 # save(file='nodes.RData', 'nodes')
